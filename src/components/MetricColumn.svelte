3937621eeaf84b9c8c7ef33163c7564bc9475486
Mathias <mathias.skallerud@gmail.com>
2022-01-19 12:07:46 +0100

    :recycle: make slot component for column

---
+++ src/components/MetricColumn.svelte
@@ -0,0 +1,26 @@
+<div class="dimention">
+  <div id="name" class="flex-item">
+    <slot name="metric"><!-- optional fallback --></slot>
+  </div>
+  <div id="metric-input" class="flex-item">
+    <slot name="metric-input"><!-- optional fallback --></slot>
+  </div>
+  <div id="api-search" class="flex-item">
+    <slot name="api-search"><!-- optional fallback --></slot>
+  </div>
+  <div id="api-button" class="flex-item">
+    <slot name="api-button"><!-- optional fallback --></slot>
+  </div>
+</div>
+
+<style>
+  .dimention {
+    display: flex;
+    flex-direction: row;
+    align-items: baseline;
+    justify-content: center;
+  }
+  .dimention div {
+    padding: 3px;
+  }
+</style>

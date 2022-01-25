85ab28891d6f7d88e0c9e9f5c032a3459da15537
Mathias <mathias.skallerud@gmail.com>
2022-01-25 13:21:27 +0100

    :sparkles: Wrapper for MetricColumn for testing

---
+++ src/__test__/slotComponentsWorkAround/MetricColumnWrapper.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import MetricColumn from "../../components/MetricColumn.svelte";
+  export let title: string;
+  export let inputValue: number;
+  export let searxhString: string;
+  export let disabledButtonIfBothFromAPI: boolean;
+</script>
+
+<MetricColumn>
+    <span slot="metric">{title}: </span>
+    <input
+    data-testid="metric-test-input"
+      slot="metric-input"
+      bind:value={inputValue}
+      type="number"
+      id="width"
+      placeholder="width"
+    />
+    <input
+      slot="api-search"
+      bind:value={searxhString}
+      type="text"
+      placeholder="bookshelf"
+      disabled={disabledButtonIfBothFromAPI}
+    />
+    <button
+      slot="api-button"
+      on:click={() =>{
+       // fetchPicture($searchWidth).then((res) =>
+       //   assignAPIValues(res, "width")
+       // )
+        }}
+      type="submit"
+      disabled={disabledButtonIfBothFromAPI}>Get from REST API</button
+    >
+</MetricColumn>
